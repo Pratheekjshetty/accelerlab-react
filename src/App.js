d7844a741ae2d81cf9911a1ccb6b7080be2ad2cd
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-06-04 11:53:19 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from "react";
+//import Test from "./test";
 import Parent from "./Parent";
 import Child from "./Child";
-//import Test from "./test";
-//import Parent from "./Parent";
 function App() {
   // x=5;
   // let text="Goodbye";
   // if(x<10){
   //   text="Hello";
+
   // const handleSubmit=(a)=>{
   //   //alert("This is Accelerlab Solutions")
   //   alert(a)
-  // }
-  let demo=false;
-  return(
-    <div>{demo?<Parent/>:<Child/>}</div>
-  )
-//   if (demo){
-//   return <Parent/>
-// }else{
-//   return <Child/>
-//   }
+
+  //   if (demo){
+  //   return <Parent/>
+  // }else{
+  //   return <Child/>
+  //   }
+
   // return (
   //   <div>
   //     {/* <h1>AccelerlabSolutions {10+10} Moodabidire</h1>
@@ -32,5 +29,10 @@ function App() {
   //   {/* <button onClick={handleSubmit("Click")}>Submit</button> */}
   //   </div>
   // );
+
+  let demo=false;
+  return(
+    <div>{demo?<Parent/>:<Child/>}</div>
+  )
 }
 export default App;
